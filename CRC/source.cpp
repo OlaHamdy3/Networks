f571b53cbf63e11abaa5e6e98ac55ecd840991ec
AliaaMahmoud <34009995+AliaaMahmoud@users.noreply.github.com>
2018-11-23 00:41:35 +0200

    the main function

--- CRC/source.cpp
+++ CRC/source.cpp
@@ -1,3 +1,9 @@
 int main(void) {
-	
+	ErororDetectionuUingCRC c;
+	c.set();
+	string frame= c.CalculationOfCRC();
+	cout << "The final frame to be sent is: " << frame << endl;
+	frame = alter(frame);
+	c.verify(frame);
+	return 0;
 }
